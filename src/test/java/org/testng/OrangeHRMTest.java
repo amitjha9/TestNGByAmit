2cd6994c53a790eea408b91bcf10f7c5b91dd81e
amit.jha <amit.jha@gdn-commerce.com>
2024-01-07 21:14:05 +0530

    Adding one new test case

---
+++ src/test/java/org/testng/OrangeHRMTest.java
@@ -0,0 +1,54 @@
+package org.testng;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class OrangeHRMTest {
+
+    public static WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().deleteAllCookies();
+    }
+
+    @Test
+    public void getPageTitleTest() {
+        String pageTitle = driver.getTitle();
+        Assert.assertEquals(pageTitle, "OrangeHRM");
+    }
+
+    @Test
+    public void verifyLoginTest() {
+        WebElement userName = driver.findElement(By.xpath("//input[@name=\"username\"]"));
+        userName.sendKeys("Admin");
+
+        WebElement password = driver.findElement(By.xpath("//input[@name=\"password\"]"));
+        password.sendKeys("admin123");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type=\"submit\"]"));
+        loginButton.click();
+
+        String pageTitle = driver.getTitle();
+        Assert.assertEquals(pageTitle, "OrangeHRM");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
